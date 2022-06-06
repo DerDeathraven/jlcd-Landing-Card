e810cb4476e29af6cd9ce1c87527bab85a9edad7
Deathraven <Lucky43@me.com>
2022-06-06 22:40:23 +0200

    improved links

--- js/main.js
+++ js/main.js
@@ -3,4 +3,17 @@ $(document).ready(function() {
 
         window.location.assign('https://jlcd.de/de/stable/')
     }
+
+    $(".portfolio").on("click", f=>{
+        window.location.href = "https://jlcd.de"
+    })
+    $(".linkedin").on("click",e=>{
+        window.location.href = "https://www.linkedin.com/in/jean-luc-christoph-dittler-b95064137/"
+    })
+    $(".github").on("click",e=>{
+        window.location.href = "https://github.com/DerDeathraven"
+    })
+    $(".mail").on("click",e=>{
+        window.location.href = "mailto:jean-luc@jlcd.de"
+    })
 })
