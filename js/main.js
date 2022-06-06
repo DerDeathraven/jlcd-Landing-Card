754b79fc7584572065622a91f1aea7254917572d
Deathraven <Lucky43@me.com>
2022-06-06 22:59:04 +0200

    smaller fixes

--- js/main.js
+++ js/main.js
@@ -4,16 +4,20 @@ $(document).ready(function() {
         window.location.assign('https://jlcd.de/de/stable/')
     }
 
-    $(".portfolio").on("click", f=>{
+    $(".portfolio").on("click", e=>{
+        $(e.currentTarget).addClass("clicked")
         window.location.href = "https://jlcd.de"
     })
     $(".linkedin").on("click",e=>{
+        $(e.currentTarget).addClass("clicked")
         window.location.href = "https://www.linkedin.com/in/jean-luc-christoph-dittler-b95064137/"
     })
     $(".github").on("click",e=>{
+        $(e.currentTarget).addClass("clicked")
         window.location.href = "https://github.com/DerDeathraven"
     })
     $(".mail").on("click",e=>{
+        $(e.currentTarget).addClass("clicked")
         window.location.href = "mailto:jean-luc@jlcd.de"
     })
 })
